abb62153b98cbff94181a719766c28a9565e1130
Pranab Quila <pranab.quila@gmail.com>
2025-12-25 22:31:24 +0530

    Refactor server setup and add error handling scaffolding
    Reorganized Express server initialization, added a 404 handler, and prepared an error handler (currently commented out). Server startup is now wrapped in a function with error logging. Improved middleware order and logging for better maintainability.

--- src/server.js
+++ src/server.js
@@ -1,26 +1,56 @@
-import express from "express";
+import express, { json } from "express";
 import cors from "cors";
 import helmet from "helmet";
 import dotenv from "dotenv";
 import logger from "./config/logger.js";
 dotenv.config();
-const app = express();
-app.use(helmet());
-app.use(cors());
 
+//Express server
+const app = express();
 const port = process.env.PORT || 3000;
 
-//Request logging
-app.use((req,res,next)=>{
-    logger.debug(`[${new Date().toISOString()}] ${req.method} ${req.url}`)
-})
+// Core middlewares
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
 
+
+
+
+//Request logging
+app.use((req, res, next) => {
+  logger.debug(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+  next();
+});
 
 //Health check endpoint
 app.get("/health", (req, res) => {
-  res.status(200).json({status: "ok",});
+  res.status(200).json({ status: "ok" });
 });
 
-app.listen(port, () => {
-  console.log(`Server running ${port}`);
+//404 handler
+app.use((req, res, next) => {
+  logger.warn(`Resource not found ${req.method} ${req.url}`);
+  res.status(404), json({ message: "resource not found" });
+  next()
 });
+
+//Error handler
+// app.use((error, req, res, next) => {
+//   logger.error(`error `, error);
+//   res.status(500), json({ message: "internal server error" });
+// });
+
+function startServer() {
+  try {
+    app.listen(port, () => {
+      logger.info(`Server running on ${port}`);
+    });
+  } catch (error) {
+    logger.error(`Failed to start the server`, error);
+    process.exit(1);
+  }
+}
+
+//start server function
+startServer();
