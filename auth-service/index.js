5888ac7ec33b62efba52744851a4404826c89245
imPranab14 <pranab.quila@gmail.com>
2026-01-01 15:50:15 +0530

    Auth service add api gateway token

--- auth-service/index.js
+++ auth-service/index.js
@@ -15,7 +15,7 @@ app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
 
-//app.use(verifyGatewayToken)
+app.use(verifyGatewayToken)
 //MongoDb Connection Function
 mongodbConnection()
 
--- auth-service/src/middleware/verifyAPIGateway.middleware.js
+++ auth-service/src/middleware/verifyAPIGateway.middleware.js
@@ -1,13 +1,16 @@
 function verifyGatewayToken(req, res, next) {
-  const token = req.headers;
-  console.log("internal_token",token);
-  if (!token){
-    return res.status(403).send({ message: "not token" });
+  const token = req.headers["x-internal-token"];
+
+  if (!token) {
+    return res.status(403).send({ message: "Forbidden – Missing gateway token" });
   }
-  if (token != process.env.INTERNAL_SERVICE_TOKEN){
-    return  res.status(403).send({ message: "Forbidden – Internal access only" });
+
+  if (token != process.env.INTERNAL_SERVICE_TOKEN) {
+    return res
+      .status(403)
+      .send({ message: "Forbidden – Internal access only" });
   }
-   
   next();
 }
+
 export default verifyGatewayToken;
