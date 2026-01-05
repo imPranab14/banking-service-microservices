265be6d7e7150518e303ab674a9fd2e6169c6299
imPranab14 <pranab.quila@gmail.com>
2026-01-05 18:00:59 +0530

    zod install

--- banking-service-frontend/src/features/auth/Login.jsx
+++ banking-service-frontend/src/features/auth/Login.jsx
@@ -2,19 +2,26 @@ import { useState } from "react";
 import { loginUser } from "../../api/auth.api";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { LoginSchema } from "../../schema/auth.schema";
 
 const Login = () => {
-  const [email, setEmail] = useState("Friedrich47@yahoo.com");
+  const [email, setEmail] = useState("Friedrich47");
   const [password, setPassword] = useState("123");
+  const [error,setError]=useState("")
   async function handelLogin(e) {
     e.preventDefault();
-    const loginData = {
+    const loginReqData = {
       email,
       password 
     };
-    console.log("loginData",loginData);
-    const loginRes = await loginUser(loginData);
-    console.log("loginRes", loginRes);
+    const data =LoginSchema.safeParse(loginReqData)
+    // if(!data.success){
+    //   return setError("sdfsdfg")
+    // }
+    console.log("loginData",data.error);
+    console.log("error",error);
+    //const loginRes = await loginUser(loginData);
+  
   }
 
   return (
