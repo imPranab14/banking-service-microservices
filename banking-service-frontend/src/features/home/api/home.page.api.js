1c86e5aa347fb028913f5c5b749f60aa29789136
Pranab Quila <pranab.quila@gmail.com>
2026-01-19 23:05:57 +0530

    Add account deletion functionality to HomePage
    Introduced a deleteAccount API method and integrated it into the HomePage component. Users can now delete accounts from the account list, which triggers a refresh of the list and displays a success notification upon successful deletion.

--- banking-service-frontend/src/features/home/api/home.page.api.js
+++ banking-service-frontend/src/features/home/api/home.page.api.js
@@ -12,4 +12,13 @@ async function listOfAccount() {
   }
 }
 
-export { listOfAccount };
+async function deleteAccount(deleteAccountNumber) {
+  try {
+    const response = await axiosInstance.delete(`api/v1/accounts/deleteAccount/?accountNumber=${deleteAccountNumber}`);
+    return response;
+  } catch (error) {
+    throw error("Delete Account Api Error")
+  }
+}
+
+export { listOfAccount,deleteAccount };
--- banking-service-frontend/src/features/home/page/HomePage.jsx
+++ banking-service-frontend/src/features/home/page/HomePage.jsx
@@ -1,39 +1,57 @@
 import { useEffect, useState } from "react";
-import { listOfAccount } from "../api/home.page.api";
+import { deleteAccount, listOfAccount } from "../api/home.page.api";
 import toast, { Toaster } from "react-hot-toast";
-import user from '../dummy/userdata.js'
+import user from "../dummy/userdata.js";
 
 function HomePage() {
   //State for Account List
   const [accountList, setAccountList] = useState();
-  const [userList,setUserList]=useState(user?.users)
+  const [userList, setUserList] = useState(user?.users);
 
   //List Account API CALL
-  useEffect(() => {
-    (async function fetchAccountList() {
-      try {
-        const response = await listOfAccount();
-        setAccountList(response?.data);
-      } catch (error) {
-        console.log("error",error);
-        toast.error(error.message || "Failed to fetch account details");
+
+  async function fetchAccountList() {
+    try {
+      const response = await listOfAccount();
+      setAccountList(response?.data);
+    } catch (error) {
+      console.log("error", error);
+      toast.error(error.message || "Failed to fetch account details");
+    }
+  }
+
+  //Delete Account
+  async function handelDeleteAccount(accountNumber) {
+    console.log("Delete Account Button", accountNumber);
+    try {
+      const response = await deleteAccount(accountNumber);
+      console.log("delete_acc", response);
+      if (response.status === 200) {
+        await fetchAccountList();
+        toast.success(`Delete account successfully ${accountNumber}`);
       }
-    })();
+    } catch (error) {
+      alert(error);
+      console.log("delete", error);
+    }
+  }
+  //Api Call
+  useEffect(() => {
+    fetchAccountList();
   }, []);
 
   console.log("accountList", accountList);
   return (
     <>
       <div className="min-h-screen bg-gray-100 p-6">
-        <div className="max-w-4xl mx-auto">
+        <div className="max-w-7xl mx-auto">
           <h1 className="text-2xl font-semibold text-gray-800 mb-6">
             Account List
           </h1>
-
           <div className="grid gap-6 md:grid-cols-2">
-            {accountList?.map((account) => (
+            {accountList?.map((account, id) => (
               <div
-                key={account._id}
+                key={id}
                 className="bg-white rounded-xl shadow-md p-6 border border-gray-200"
               >
                 <div className="flex justify-between items-center mb-4">
@@ -77,17 +95,22 @@ function HomePage() {
                 <button className="mt-5 w-full bg-indigo-600 text-white py-2 rounded-lg hover:bg-indigo-700 transition">
                   View Details
                 </button>
+                <button
+                  onClick={() => handelDeleteAccount(account.accountNumber)}
+                >
+                  Delete Account
+                </button>
               </div>
             ))}
           </div>
 
-          {userList.map((ele,id)=>{
+          {/* {userList.map((ele,id)=>{
             return(
               <>
               <h1 key={id}>Name : {ele.firstName} {ele.lastName}</h1>
               </>
             )
-          })}
+          })} */}
         </div>
       </div>
       <Toaster />
