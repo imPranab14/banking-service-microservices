9413f912c0dc7c0072ae9378eb5a84181fb7e67b
imPranab14 <pranab.quila@gmail.com>
2026-02-11 23:09:44 +0530

    tenstack query isFetching added

--- banking-service-frontend/src/features/home/page/SlowResponsePage.jsx
+++ banking-service-frontend/src/features/home/page/SlowResponsePage.jsx
@@ -4,17 +4,24 @@ import { slowListOfTransaction } from "../api/home.page.api";
 
 function SlowResponsePage() {
   // NOTE Queries
-  const { data: transactionData } = useQuery({
+  const { data: transactionData, isFetching } = useQuery({
     queryKey: ["allTransactionList"],
     queryFn: slowListOfTransaction,
+      staleTime: 1000 * 60 * 5, // 5 minutes
+
   });
 
   console.log("Slow_Api_Repose_Transaction", transactionData);
 
   return (
     <>
-      <h1>email:{transactionData?.email}</h1>
-      <h1>accountNumber:{transactionData?.accountNumber}</h1>
+      {isFetching && <p>Loading....</p>}
+      {!isFetching && (
+        <>
+          <h1>email:{transactionData?.email}</h1>
+          <h1>accountNumber:{transactionData?.accountNumber}</h1>
+        </>
+      )}
     </>
   );
 }
